983ab15e83a66ba72861285453ffcfb7b980aabc
hasan <hsnakbba@gmail.com>
2023-05-13 18:46:53 -0400

    Hat method created

---
+++ src/MainPackage/Hat.java
@@ -0,0 +1,18 @@
+package MainPackage;
+
+public class Hat {
+
+    public static void main(String[] args) {
+
+        String hat;
+        String hot;
+        int temp = 0;
+
+        if(temp>=80){
+            System.out.println("Wear your hat");
+        }else{
+            System.out.println("Don't wear it");
+        }
+
+    }
+}
