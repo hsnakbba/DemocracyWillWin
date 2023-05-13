a50c1439857f226bb8a5518e7d49d8b49bd6e19b
hasan <hsnakbba@gmail.com>
2023-05-13 18:30:31 -0400

    Water method created and made some changes

---
+++ src/MainPackage/Water.java
@@ -0,0 +1,14 @@
+package MainPackage;
+
+public class Water {
+
+    public String bin;
+    public String bottle;
+    public String glass;
+
+    public Water(String bin, String bottle, String glass) {
+        this.bin = bin;
+        this.bottle = bottle;
+        this.glass = glass;
+    }
+}
