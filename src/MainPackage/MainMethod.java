bd189f84245d069b57771e24ba32d4d1b1209f95
hasan <hsnakbba@gmail.com>
2023-05-13 18:26:43 -0400

    Main Method created

---
+++ src/MainPackage/MainMethod.java
@@ -0,0 +1,12 @@
+package MainPackage;
+
+public class MainMethod {
+
+    public static void main(String[] args) {
+
+        // new features added in this file
+
+        // more coming
+
+    }
+}
